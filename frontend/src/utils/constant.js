4e4669c0f181c5894cf4334b626147829ff69a5f
Pankajyadav-123 <Pankajyadav95834@gmail.com>
2026-01-25 13:45:27 +0530

    url upadte

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+export const USER_API_END_POINT = 'https://webjob-1.onrender.com/api/v1/user';
+export const JOB_API_END_POINT = 'https://webjob-1.onrender.com/api/v1/job';
+export const APPLICATION_API_END_POINT = 'https://webjob-1.onrender.com/api/v1/application';
+export const COMPANY_API_END_POINT = 'https://webjob-1.onrender.com/api/v1/company';
