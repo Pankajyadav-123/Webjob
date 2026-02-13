e9b081312d6f1d18b6780def31b8c063abf609b4
Pankajyadav-123 <Pankajyadav95834@gmail.com>
2026-02-13 09:49:50 +0530

    browse error fix

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -18,13 +18,11 @@ const Browse = () => {
         }
     },[])
     return (
-       
-        
         <div>
-           <Navbar />
-            <div className='max-w-7xl mx-auto my-10'>
-                <h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
-                <div className='grid grid-cols-3 gap-4'>
+            <Navbar />
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-10'>
+                <h1 className='font-bold text-lg sm:text-xl mb-6'>Search Results ({allJobs.length})</h1>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                     {
                         allJobs.map((job) => {
                             return (
@@ -33,11 +31,9 @@ const Browse = () => {
                         })
                     }
                 </div>
-
             </div>
             <Footer />
         </div>
-       
     )
 }
 
